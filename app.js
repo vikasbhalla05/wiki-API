0273d5e840d576db2f183aea7e73b8553418e0ea
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2022-03-30 12:29:45 +0530

    setting up api with the help of postman in blog get, post and delete done

---
+++ app.js
@@ -0,0 +1,60 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+let ejs = require('ejs');
+let app = express();
+
+app.set('view-engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+mongoose.connect('mongodb://localhost:27017/WikiDB', {useNewUrlParser: true});
+
+let articleSchema = mongoose.Schema({
+	title: { type: String },
+	content: { type: String }
+});
+
+let article = mongoose.model('article', articleSchema);
+
+
+
+// Chained Routes
+app.route('/articles')
+.get(function(req, res){
+	article.find({}, function( err, articleFound){
+		res.send(articleFound);
+	})
+})
+.post(function(req, res, err){
+	console.log(req.body.title);
+	console.log(req.body.content);
+	let newPost = new article ({
+		title: req.body.title,
+		content: req.body.content
+	});
+
+	newPost.save(function (err){
+		if(!err){
+			res.send('Success message');
+		}
+		else{
+			res.send(err);
+		}
+	})
+})
+.delete(function(req, res) {
+	article.deleteMany( function(err){
+		if(!err){
+			res.send('Deletion Success');
+		}
+		else{
+			res.send(err);
+		}
+	});
+});
+
+
+app.listen(3000, function() {
+	console.log("app listening on port 3000");
+});
